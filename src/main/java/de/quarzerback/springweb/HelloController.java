b1abf0be1f552fba4d2c68654cad57eed3d7c035
Quarzerback <157362104+Quarzerback@users.noreply.github.com>
2024-02-14 11:55:14 +0100

    Added the challenges

--- src/main/java/de/quarzerback/springweb/HelloController.java
+++ src/main/java/de/quarzerback/springweb/HelloController.java
@@ -1,15 +1,48 @@
 package de.quarzerback.springweb;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
-@RequestMapping("/api/hello")
+@RequestMapping("/api")
 public class HelloController {
-
-    @GetMapping
+    List<Message> messageList = new ArrayList<>();
+    @GetMapping("/hello")
     public String hello() {
         return "Hello, World!";
+    }
+
+    @GetMapping("/hello/{name}")
+    public String hello(@PathVariable String name) {
+        return "Hello, " + name + "!";
+    }
+
+    @GetMapping("/messages")
+    public List<Message> getAllMessages(){
+        return messageList;
+    }
+
+    @PostMapping("/messages")
+    public List<Message> addMessage(@RequestBody Message message){
+        messageList.add(message);
+        return messageList;
+    }
+
+    @DeleteMapping("/messages/{id}")
+    public List<Message> removeMessage(@PathVariable String id) {
+        Message removalMessage = getMessageById(id);
+        if (removalMessage != null) {
+            messageList.remove(removalMessage);
+        }
+        return messageList;
+    }
+
+    private Message getMessageById(String id) {
+        return messageList.stream()
+                .filter(message -> id.equals(message.getId()))
+                .findFirst()
+                .orElse(null);
     }
 }
---
+++ src/main/java/de/quarzerback/springweb/Message.java
@@ -0,0 +1,11 @@
+package de.quarzerback.springweb;
+
+import lombok.Data;
+
+@Data
+public class Message {
+    private String name;
+    private String message;
+    private String id;
+}
+
